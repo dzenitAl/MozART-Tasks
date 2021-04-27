533ab0d4849d7488cea8eb3b067fe415c1723064
dinabjelic <75682425+dinabjelic@users.noreply.github.com>
2021-04-27 13:53:43 +0200

    memory

--- blank/src/main.ts
+++ blank/src/main.ts
@@ -3,11 +3,13 @@ import App from './components/App.vue'
 import Test from './components/Test.vue'
 import { Label } from 'tns-core-modules/ui/label';
 import { isAndroid } from 'tns-core-modules/platform';
-import DictatsCompositions from './components/DictatsCompositions.vue'
+import DictatsCompositions from './components/DictatsCompositions.vue'; 
+import Memory from './components/Memory.vue';
 
+Vue.registerElement("VideoPlayer", () => require("nativescript-videoplayer").Video);
 new Vue({
   
-  render: h => h('frame', [h(Test)])
+  render: h => h('frame', [h(DictatsCompositions)])
 }).$start()
 
 export function onLabelLoaded(args) {
